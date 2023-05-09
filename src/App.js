1be1b1d3cc2744ca28bba696b0ad859553044dee
RosheedatOyindamola <Rosheedat1234@yahoo>
2023-05-09 21:36:40 +0100

    add movie detail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import { Route, Routes } from "react-router-dom";
+import StarWars from "../src/Components/Star-Wars"
 import './App.css';
+import MovieDetail from "./Components/movieDetail";
 
 function App() {
   return (
     <div className="App">
-     
+      {/* <StarWars /> */}
+  
+      <Routes>
+        <Route path="/movie/:id" element={<MovieDetail/>}/>
+        <Route path="/" element={<StarWars/>}/>
+      </Routes>
     </div>
   );
 }
